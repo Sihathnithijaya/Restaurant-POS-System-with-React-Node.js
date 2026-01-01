5b27f78947143c804ad403931dad0d1a9c423d1f
Sihath Rathanayake <122671416+Sihathnithijaya@users.noreply.github.com>
2026-01-01 20:36:25 +0530

    feat: enhance Home component layout with scrollable section and improved structure

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -4,29 +4,36 @@ import Greetings from "../components/home/Greetings.jsx";
 import MiniCard from "../components/home/MiniCard.jsx";
 import { BsCashCoin } from "react-icons/bs";
 import { GrInProgress } from "react-icons/gr";
-
+import RecentOrders from "../components/home/RecentOrders.jsx";
+import PopulerDishes from "../components/home/PopulerDishes.jsx";
 
 
 function Home() {
     return (
         <div >
-            <section className=" bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+            <section className=" bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-scroll flex gap-3 scrollbar-hide">
                 {/* Left Side */}
                 <div className="flex-[3]">
-                    <Greetings/>
+                    <Greetings />
                     <div className="flex items-center w-full gap-3 px-8 mt-8">
-                        <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6}/>
-                        <MiniCard title="In Progress" icon={<GrInProgress />} number={20} footerNum={3.6}/>
+                        <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
+                        <MiniCard title="In Progress" icon={<GrInProgress />} number={20} footerNum={3.6} />
                     </div>
+                    <RecentOrders />
                 </div>
 
+
                 {/* Right Side */}
-                <div className="flex-[2] bg-[#161616]"></div>
-                <BottomNav />
+                <div className="flex-[2] bg-[#161616]">
+
+                    <PopulerDishes /></div>
+
+
+
             </section>
-        
+            <BottomNav />
         </div>
-        
+
     );
 }
 
